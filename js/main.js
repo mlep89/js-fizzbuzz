91dab74646c5b3c943aad23f2998f43844baf00e
Marco <mr.lepore@hotmail.it>
2021-02-25 16:24:47 +0100

    Struttura inziale

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+
+//Creo un ciclo for per stampare un elenco da 1 a 100
+for (var i = 1; i < 101; i++) {
+    console.log(i);
+    // //Introduco la condizione per individuare i multipli di 3
+    // if (i % 3 == 0) {
+    //     //Sostituisco il valore i con la parola Fizz;
+    //     i = "Fizz";
+    // }
+    // //Introduco la condizione per individuare i multipli di 5
+    // else if (i % 5 == 0) {
+    //     //Sostituisco il valore i con la parola Buzz
+    //     i = "Buzz";
+    // }
+    // //Introduco la condizione per individuare i multipli di 3 e 5
+    // else (i % 3 == 0 && i % 3 == 0) 
+    //     //Sostituisco il valore i con la parola FizzBuzz
+    //     i = "FizzBuzz";
+}
